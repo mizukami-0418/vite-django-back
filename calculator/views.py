f4b6d631e921fc409b52493c08b77f753b4ba33a
tomomi mizukami <ff10mm11yy23tamk@gmail.com>
2025-04-15 23:47:43 +0900

    calculatorアプリのURL設定と計算機機能を追加

--- calculator/views.py
+++ calculator/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class CalculatorView(APIView):
+    def post(self, request):
+        try:
+            num1 = int(request.data.get('num1'))
+            num2 = int(request.data.get('num2'))
+            operator = request.data.get('operator')
+            
+            if operator == '+':
+                result = num1 + num2
+            elif operator == '-':
+                result = num1 - num2
+            elif operator == '*':
+                result = num1 * num2
+            elif operator == '/':
+                if num2 == 0:
+                    return Response({"error": "0で割ることはできません"}, status=status.HTTP_400_BAD_REQUEST)
+                result = round(num1 / num2, 2)
+            else:
+                return Response({"error": "無効な演算子"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"result": result}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
